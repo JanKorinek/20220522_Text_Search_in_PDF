aea214f406b92c83219687d1f126b4da0a6671d0
Jan Korinek <korinek.j@gmail.com>
2022-05-22 22:31:42 +0200

    Keyword search within multiple PDFs implemented. Multiprocessing implemeted.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import PyPDF2
+import time
 import re
+import os
+import PyPDF2
+import pikepdf
 import webbrowser
+
 import pandas as pd
+import multiprocessing as mp
+
+from PyPDF2.errors import PdfReadError
+
+
+def repare_pdf(file):
+    pdf = file.split('/')
+    print("Repairing following PDF: {0}".format(pdf[-1]))
+    pdf = pikepdf.Pdf.open(file)
+    pdf.save(file + '.tmp')
+    pdf.close()
+    os.unlink(file)
+    os.rename(file + '.tmp', file)
+
 
 def generate_html(dataframe: pd.DataFrame, *args):
     # get the table HTML from the dataframe
@@ -33,13 +51,26 @@ def generate_html(dataframe: pd.DataFrame, *args):
     """
     return html
 
-def search_pdf(reader, keyword, path):
+
+def search_pdf(keyword, path):
+    # Reader initiation
+    f = open(path, 'rb')
+    try:
+        reader = PyPDF2.PdfFileReader(f)
+    except PdfReadError:
+        repare_pdf(path)
+        f = open(path, 'rb')
+        reader = PyPDF2.PdfFileReader(f)
+
     pdf = path.split('/')
     results = []
     for p in range(reader.numPages):
         print(f'Checking page {p} in [{pdf[-1]}]')
         page = reader.getPage(p)
-        text = page.extractText()
+        try:
+            text = page.extractText()
+        except:
+            continue
 
         for line in text.splitlines():
             if re.findall(keyword, line, flags=re.IGNORECASE):
@@ -51,26 +82,47 @@ def search_pdf(reader, keyword, path):
                 }
                 results.append(result)
 
-    print(f'All of {reader.numPages} pages checked. Search complete.')
+    f.close()
+    print(f'All of {reader.numPages} pages checked.')
 
     return results
 
 
+def run_parallel_pool(keyword, pdfs):
+    with mp.Pool(mp.cpu_count()) as pool:
+        res = pool.starmap(search_pdf, zip([keyword] * len(pdfs), pdfs))
+        pool.close()
+    return res
+
+
 if __name__ == "__main__":
-    path = '/home/p51/PycharmProjects/20220522_Text_Search_in_PDF/pdfs/Artificial Intelligence for Cloud and Edge Computing (2022).pdf'
-    keyword = 'recommend'
+    keyword = 'Terraform'
+    # folder = 'pdfs'
+    folder = '/media/p51/Data/Data/Library/_Computer_Science/_Projects_Selection/'
+    parallel_processing = True
 
-    # Reader initiation
-    f = open(path, 'rb')
-    reader = PyPDF2.PdfFileReader(f)
+    # Measure time
+    t0 = time.time()
 
-    # Search PDF
-    results = search_pdf(reader, keyword, path)
-    f.close()
+    # List all available PDFs in specified folder
+    pdfs = [os.path.abspath(os.path.join(root, name))
+            for root, dirs, files in os.walk(folder) for name in files if name.endswith('pdf')]
 
-    df = pd.DataFrame(results)
+    # Search keywords in PDFs
+    if parallel_processing:
+        results_flat = [item for sublist in run_parallel_pool(keyword, pdfs) for item in sublist]
+    else:
+        results_flat = [item for sublist in [search_pdf(keyword, path) for path in pdfs] for item in sublist]
+
+    results_df = pd.DataFrame(results_flat)
 
     # Generate and save HTML
-    html = generate_html(df, keyword)
+    html = generate_html(results_df, keyword)
     open("results/search_results.html", "w").write(html)
     webbrowser.open("results/search_results.html")
+
+    # Evaluate time
+    t1 = time.time() - t0
+    print("Time elapsed (s): ", round(t1, 0))
+    print("Time elapsed (min): ", round(t1/60, 1))
+    print("Time elapsed (h): ", round(t1/3600, 2))
